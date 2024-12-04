b8e0e386ab61ace8fe18938751e945094538a756
Rudy Gabuco Jr <rudyjr.gabuco@gmail.com>
2024-12-04 01:09:17 -0700

    add two more functions

--- client/src/utils/authUtils.js
+++ client/src/utils/authUtils.js
@@ -20,3 +20,33 @@ export const checkUserLoggedIn = () => {
         return null;
     }
 };
+
+export const getUserLoggedIn = () => {
+    const token = localStorage.getItem("token");
+    if (token) {
+        try {
+            const decodedToken = jwtDecode(token);
+            return decodedToken.username;
+        } catch (error) {
+            console.error("Error decoding token:", error);
+            return null;
+        }
+    } else {
+        return null;
+    }
+};
+
+export const isAdministrator = () => {
+    const token = localStorage.getItem("token");
+    if (token) {
+        try {
+            const decodedToken = jwtDecode(token);
+            return decodedToken.isAdmin;
+        } catch (error) {
+            console.error("Error decoding token:", error);
+            return false;
+        }
+    } else {
+        return false;
+    }
+};
